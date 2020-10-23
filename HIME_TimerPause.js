fa77212e9721ca38a06eddbfa098b115fe0687b1
munokura <36838778+munokura@users.noreply.github.com>
2020-10-23 13:09:01 +0900

    MZでの動作確認

--- HIME_TimerPause.js
+++ HIME_TimerPause.js
@@ -1,4 +1,69 @@
-/*:
+/*:ja
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/HIME-MV-plugins-jp/master/HIME_TimerPause.js
+ * @title Timer Pause
+ * @author Hime --> HimeWorks (http://himeworks.com)
+ * @date Nov 18, 2015
+ * @filename HIME_TimerPause.js
+ *
+ * @plugindesc v1.0 タイマーの一時停止と再開ができます。タイマーを一時停止すると、画面上には残りますが、カウントダウンはしません
+ *
+ * @help
+ * 翻訳:ムノクラ
+ * https://fungamemake.com/
+ * https://twitter.com/munokura/
+ *
+ * 元プラグイン:
+ * http://himeworks.com/2015/11/timer-pause/
+ * 
+ * == 説明 ==
+ *
+ * タイマーのカウントダウンを一時的に停止させても、
+ * 画面に表示させたいと思いませんか?
+ * このプラグインは、
+ * ゲームタイマーを簡単に一時停止したり再開したりできる
+ * 2つのスクリプトコールを提供します。
+ *
+ * == 利用規約 ==
+ *
+ * - クレジットを表示する非営利プロジェクトでの使用は無料
+ * - 商用利用の場合、私に連絡してください
+ *
+ * == Change Log ==
+ *
+ * Nov 18, 2015 -  initial release
+ *
+ * == 使用方法 ==
+ *
+ * タイマーを一時停止するスクリプト
+ *
+ *   $gameTimer.pause();
+ *
+ * タイマーを再開するスクリプト
+ *
+ *   $gameTimer.resume();
+ *
+ * 通常のタイマーで使用できます。
+ *
+ */
+/*
+ * あなたが私の仕事を楽しんでいるなら、
+ * パトレオンで私への支援を検討してください！
+ *
+ * - https://www.patreon.com/himeworks
+ *
+ * ご質問や懸念がある場合、
+ * 次のサイトのいずれかで私に連絡できます。
+ *
+ * - Main Website: http://himeworks.com
+ * - Facebook: https://www.facebook.com/himeworkscom/
+ * - Twitter: https://twitter.com/HimeWorks
+ * - Youtube: https://www.youtube.com/c/HimeWorks
+ * - Tumblr: http://himeworks.tumblr.com/
+ *
+*/
+
+ /*:
 @title Timer Pause
 @author Hime --> HimeWorks (http://himeworks.com)
 @date Nov 18, 2015
@@ -50,65 +115,6 @@ To resume the timer, use the script call
   
 Note that this can be used on any Game_Timer object.
 
- */
-/*:ja
- * @title Timer Pause
- * @author Hime --> HimeWorks (http://himeworks.com)
- * @date Nov 18, 2015
- * @filename HIME_TimerPause.js
- * @url http://himeworks.com/2015/11/timer-pause/
- *
- * あなたが私の仕事を楽しんでいるなら、
- * パトレオンで私への支援を検討してください！
- *
- * - https://www.patreon.com/himeworks
- *
- * ご質問や懸念がある場合、
- * 次のサイトのいずれかで私に連絡できます。
- *
- * - Main Website: http://himeworks.com
- * - Facebook: https://www.facebook.com/himeworkscom/
- * - Twitter: https://twitter.com/HimeWorks
- * - Youtube: https://www.youtube.com/c/HimeWorks
- * - Tumblr: http://himeworks.tumblr.com/
- *
- * @plugindesc v1.0 タイマーの一時停止と再開ができます。タイマーを一時停止すると、画面上には残りますが、カウントダウンはしません
- *
- * @help
- * 翻訳:ムノクラ
- * https://fungamemake.com/
- * https://twitter.com/munokura/
- *
- *
- * == 説明 ==
- *
- * タイマーのカウントダウンを一時的に停止させても、
- * 画面に表示させたいと思いませんか?
- * このプラグインは、
- * ゲームタイマーを簡単に一時停止したり再開したりできる
- * 2つのスクリプトコールを提供します。
- *
- * == 利用規約 ==
- *
- * - クレジットを表示する非営利プロジェクトでの使用は無料
- * - 商用利用の場合、私に連絡してください
- *
- * == Change Log ==
- *
- * Nov 18, 2015 -  initial release
- *
- * == 使用方法 ==
- *
- * タイマーを一時停止するスクリプト
- *
- *   $gameTimer.pause();
- *
- * タイマーを再開するスクリプト
- *
- *   $gameTimer.resume();
- *
- * 通常のタイマーで使用できます。
- *
  */
 
 var Imported = Imported || {};
