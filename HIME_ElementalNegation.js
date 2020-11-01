b42303e14a2b4b644d11bcedad76c830e78befc5
munokura <munokura@users.noreply.github.com>
2020-11-01 10:32:29 +0900

    MZで動作確認

--- HIME_ElementalNegation.js
+++ HIME_ElementalNegation.js
@@ -1,4 +1,80 @@
-﻿/*:
+﻿/*:ja
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/HIME-MV-plugins-jp/master/HIME_ElementalNegation.js
+ * @title Elemental Negation
+ * @author Hime --> HimeWorks (http://himeworks.com)
+ * @version 1.0
+ * @date Dec 4, 2015
+ * @filename HIME_ElementalNegation.js
+ *
+ * @plugindesc 負の属性有効度を指定できます。
+ * @help
+ * 翻訳:ムノクラ
+ * https://fungamemake.com/
+ * https://twitter.com/munokura/
+ *
+ * 元プラグイン:
+ * http://himeworks.com/2015/12/elemental-negation/
+ * 
+ * == 説明 ==
+ *
+ * 属性有効度は、スキルまたはアイテムのダメージに対する乗数効果を決定します。
+ *
+ * 例えば、炎に対して200%の属性有効度がある場合、
+ * 与えられる炎によるダメージは200%の効果/2倍のダメージになります。
+ *
+ * 同様に、炎に対する属性有効度が0%である場合、
+ * 与えられる炎によるダメージは0%の効果/ダメージなしになります。
+ *
+ * しかし、属性有効度が負の場合はどうなりますか?
+ * 実際にダメージを吸収することを意味します。
+ *
+ * 炎に対して-100%の属性有効度は、全てのダメージを吸収することを意味し、
+ * -50%の属性有効度は、ダメージの半分を吸収することを意味します。
+ *
+ * このプラグインを使用すると、負の属性有効度を指定できます。
+ *
+ * == 使用法 ==
+ *
+ * 属性有効度を持たせたいオブジェクトにはメモタグを入れます。
+ *
+ *   <element rate: ELEMENT_TYPE, RATE% />
+ *
+ * ELEMENT_TYPEは属性の名前(1バイト文字のみ)/IDであり、RATEは数値です。
+ *
+ * 例えば、炎の属性は、用語のデータベースにID2であり、
+ * 炎を100%吸収するには、次を記述します。
+ *
+ *   <element rate: 2, -100% />
+ *
+ *
+ * == 利用規約 ==
+ *
+ * - クレジットを表示する非営利プロジェクトでの使用は無料
+ * - 商用プロジェクトでの使用は連絡してください
+ *
+ * == Change Log ==
+ *
+ * 1.0 Dec 4, 2015
+ *  - initial release
+ */
+/*
+ * あなたが私の仕事を楽しんでいるなら、
+ * パトレオンで私への支援を検討してください！
+ *
+ * * https://www.patreon.com/himeworks
+ *
+ * ご質問や懸念がある場合、
+ * 次のサイトのいずれかで私に連絡できます。
+ *
+ * * Main Website: http://himeworks.com
+ * * Facebook: https://www.facebook.com/himeworkscom/
+ * * Twitter: https://twitter.com/HimeWorks
+ * * Youtube: https://www.youtube.com/c/HimeWorks
+ * * Tumblr: http://himeworks.tumblr.com/
+ */
+
+/*:
 @title Elemental Negation
 @author Hime --> HimeWorks (http://himeworks.com)
 @version 1.0
@@ -47,13 +123,13 @@ This plugin allows you to specify negative element rates.
 == Change Log ==
 
 1.0 Dec 4, 2015
- - initial release
+- initial release
 
 == Usage ==
 
 Note-tag any objects that have traits with
 
-  <element rate: ELEMENT_TYPE, RATE% />
+ <element rate: ELEMENT_TYPE, RATE% />
 
 Where the ELEMENT_TYPE is either the name or the ID of the element, and
 the RATE is a number.
@@ -61,80 +137,10 @@ the RATE is a number.
 For example, if the Fire element is ID 2 in your terms database and you want
 to absorb 100% of fire, you can say either of the following:
 
-  <element rate: Fire, -100% />
-  <element rate: 2, -100% />
+ <element rate: Fire, -100% />
+ <element rate: 2, -100% />
 
- */
-/*:ja
- * @title Elemental Negation
- * @author Hime --> HimeWorks (http://himeworks.com)
- * @version 1.0
- * @date Dec 4, 2015
- * @filename HIME_ElementalNegation.js
- * @url http://himeworks.com/2015/12/elemental-negation/
- *
- * あなたが私の仕事を楽しんでいるなら、
- * パトレオンで私への支援を検討してください！
- *
- * * https://www.patreon.com/himeworks
- *
- * ご質問や懸念がある場合、
- * 次のサイトのいずれかで私に連絡できます。
- *
- * * Main Website: http://himeworks.com
- * * Facebook: https://www.facebook.com/himeworkscom/
- * * Twitter: https://twitter.com/HimeWorks
- * * Youtube: https://www.youtube.com/c/HimeWorks
- * * Tumblr: http://himeworks.tumblr.com/
- *
- * @plugindesc 負の属性有効度を指定できます。
- * @help
- * 翻訳:ムノクラ
- * https://fungamemake.com/
- * https://twitter.com/munokura/
- *
- * == 説明 ==
- *
- * 属性有効度は、スキルまたはアイテムのダメージに対する乗数効果を決定します。
- *
- * 例えば、炎に対して200%の属性有効度がある場合、
- * 与えられる炎によるダメージは200%の効果/2倍のダメージになります。
- *
- * 同様に、炎に対する属性有効度が0%である場合、
- * 与えられる炎によるダメージは0%の効果/ダメージなしになります。
- *
- * しかし、属性有効度が負の場合はどうなりますか?
- * 実際にダメージを吸収することを意味します。
- *
- * 炎に対して-100%の属性有効度は、全てのダメージを吸収することを意味し、
- * -50%の属性有効度は、ダメージの半分を吸収することを意味します。
- *
- * このプラグインを使用すると、負の属性有効度を指定できます。
- *
- * == 利用規約 ==
- *
- * - クレジットを表示する非営利プロジェクトでの使用は無料
- * - 商用プロジェクトでの使用は連絡してください
- *
- * == Change Log ==
- *
- * 1.0 Dec 4, 2015
- *  - initial release
- *
- * == 使用法 ==
- *
- * 属性有効度を持たせたいオブジェクトにはメモタグを付けます
- *
- *   <element rate: ELEMENT_TYPE, RATE% />
- *
- * ELEMENT_TYPEは属性の名前(1バイト文字のみ)/IDであり、RATEは数値です。
- *
- * 例えば、炎の属性は、用語のデータベースにID2であり、
- * 炎を100%吸収するには、次を記述します。
- *
- *   <element rate: 2, -100% />
- *
- */
+*/
 
 var Imported = Imported || {};
 var TH = TH || {};
