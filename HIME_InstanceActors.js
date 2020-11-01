5cdad976a7c58366f0ff56d9d88b2e994dfaa5c0
munokura <munokura@users.noreply.github.com>
2020-11-01 11:35:48 +0900

    MZで動作確認

--- HIME_InstanceActors.js
+++ HIME_InstanceActors.js
@@ -1,4 +1,125 @@
-﻿/*:
+﻿/*:ja
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/HIME-MV-plugins-jp/master/HIME_InstanceActors.js
+ * @title Instance Actors
+ * @author Hime --> HimeWorks (http://himeworks.com)
+ * @version 1.1
+ * @date Feb 23, 2016
+ * @filename HIME_InstanceActors.js
+ *
+ * @plugindesc v1.0 ゲーム中にカスタムアクターを作成できます。
+ * @help
+ * 翻訳:ムノクラ
+ * https://fungamemake.com/
+ * https://twitter.com/munokura/
+ *
+ * 元プラグイン:
+ * http://himeworks.com/2016/01/instance-actors/
+ *
+ * == 説明 ==
+ *
+ * RPGツクールMVのデフォルトでは、
+ * ゲーム中の新しいアクターの作成をサポートしていません。
+ *
+ * ゲームを作成する時、データベースにアクターを設定し、
+ * ゲーム中にそれらのアクターが使用されます。
+ *
+ * このプラグインを使用すると、
+ * ゲーム中に完全に新しいアクターを作成する可能性が広がります。
+ *
+ * 例えば、戦闘中に敵をキャプチャして、
+ * パーティに追加される前にアクターに変換するメカニックを作成できます。
+ * これらのアクターは、プロジェクトを最初に作成した時は存在していません。
+ * また、他の保存ファイルにも存在していません。
+ *
+ * このプラグインは、
+ * 実行時に生成されるカスタムアクターを管理するための機能を提供します。
+ *
+ * == 使用法 ==
+ *
+ * -- 設定 --
+ *
+ * プラグインパラメータで、'Template Actor ID'を選択します。
+ * 新しく作成された全てのアクターがベースとする'デフォルト'のアクターです。
+ *
+ * 次に、カスタムアクターの識別方法を表す'Start ID'を設定します。
+ * ゲームが開始すると、これらのIDを変更する方法はないため、
+ * プロジェクトにさらにアクターを追加する予定がある場合、
+ * Start IDを選択する時、それを考慮に入れる必要があります。
+ * 私は安全を考慮し1000のような大きな値をお勧めします。
+ *
+ * -- アクターの作成 --
+ *
+ * 新しいアクターを作成するには、スクリプトを使用します
+ *
+ *   var newActor = InstanceManager.addActor();
+ *
+ * テンプレートアクターをベースとして使用して、新しいアクターを作成します。
+ * 基本的には、新しいIDが付いたコピーです。
+ *
+ * 別のテンプレートをベースにしたい場合、次のスクリプトで指定できます。
+ *
+ *   var newActor = InstanceManager.addActor( 2 );
+ *
+ * テンプレートとしてアクター2を使用します。
+ * アクター作成プロセスは、単にアクターを作成するだけです。
+ * パーティに自動的に追加されるわけではありません。
+ * したくないかもしれないからです。
+ *
+ * パーティーに追加したい場合
+ *
+ *   var newActor = InstanceManager.addActor();
+ *   $gameParty.addActor(newActor.id);
+ *
+ * -- アクターの削除 --
+ *
+ * このプラグインは、他のオブジェクトによって使用されている可能性があるため、
+ * アクターの削除をサポートしていません。
+ * アクターを使用しない方が安全です。
+ *
+ *
+ * == 利用規約 ==
+ *
+ * - クレジットを表示する非営利プロジェクトでの使用は無料
+ * - 商用利用の場合、私に連絡してください
+ *
+ * == Change Log ==
+ *
+ * 1.1 - Feb 23, 2016
+ *  * read plugin parameters
+ * 1.0 - Jan 23, 2016
+ *  * initial release
+ * 
+ * 
+ * @param Template Actor ID
+ * @text テンプレートアクターID
+ * @desc 新しく作成される全アクターのベースとなるデフォルトのアクター
+ * @type number
+ * @default 1
+ *
+ * @param Start ID
+ * @text 開始ID
+ * @desc 新しいアクターに割り当てるID。ゲームの開始後は変更できないため、大きな値をお勧めします。
+ * @type number
+ * @default 1000
+ */
+/*
+ * あなたが私の仕事を楽しんでいるなら、
+ * パトレオンで私への支援を検討してください！
+ *
+ * - https://www.patreon.com/himeworks
+ *
+ * ご質問や懸念がある場合、
+ * 次のサイトのいずれかで私に連絡できます。
+ *
+ * - Main Website: http://himeworks.com
+ * - Facebook: https://www.facebook.com/himeworkscom/
+ * - Twitter: https://twitter.com/HimeWorks
+ * - Youtube: https://www.youtube.com/c/HimeWorks
+ * - Tumblr: http://himeworks.tumblr.com/
+ */
+
+/*:
  * @title Instance Actors
  * @author Hime --> HimeWorks (http://himeworks.com)
  * @version 1.1
@@ -101,118 +222,6 @@
  * @param Start ID
  * @desc The ID that the game should assign to new actors. This cannot be changed
  * once a game begins, so a larger value is recommended.
- * @default 1000
- */
-/*:ja
- * @title Instance Actors
- * @author Hime --> HimeWorks (http://himeworks.com)
- * @version 1.1
- * @date Feb 23, 2016
- * @filename HIME_InstanceActors.js
- * @url http://himeworks.com/2016/01/instance-actors/
- *
- * あなたが私の仕事を楽しんでいるなら、
- * パトレオンで私への支援を検討してください！
- *
- * - https://www.patreon.com/himeworks
- *
- * ご質問や懸念がある場合、
- * 次のサイトのいずれかで私に連絡できます。
- *
- * - Main Website: http://himeworks.com
- * - Facebook: https://www.facebook.com/himeworkscom/
- * - Twitter: https://twitter.com/HimeWorks
- * - Youtube: https://www.youtube.com/c/HimeWorks
- * - Tumblr: http://himeworks.tumblr.com/
- *
- * @plugindesc 1.0 ゲーム中にカスタムアクターを作成できます。
- * @help
- * 翻訳:ムノクラ
- * https://fungamemake.com/
- * https://twitter.com/munokura/
- *
- *
- * == 説明 ==
- *
- * RPGツクールMVのデフォルトでは、
- * ゲーム中の新しいアクターの作成をサポートしていません。
- *
- * ゲームを作成する時、データベースにアクターを設定し、
- * ゲーム中にそれらのアクターが使用されます。
- *
- * このプラグインを使用すると、
- * ゲーム中に完全に新しいアクターを作成する可能性が広がります。
- *
- * 例えば、戦闘中に敵をキャプチャして、
- * パーティに追加される前にアクターに変換するメカニックを作成できます。
- * これらのアクターは、プロジェクトを最初に作成した時は存在していません。
- * また、他の保存ファイルにも存在していません。
- *
- * このプラグインは、
- * 実行時に生成されるカスタムアクターを管理するための機能を提供します。
- *
- * == 利用規約 ==
- *
- * - クレジットを表示する非営利プロジェクトでの使用は無料
- * - 商用利用の場合、私に連絡してください
- *
- * == Change Log ==
- *
- * 1.1 - Feb 23, 2016
- *  * read plugin parameters
- * 1.0 - Jan 23, 2016
- *  * initial release
- *
- * == 使用法 ==
- *
- * -- 設定 --
- *
- * プラグインパラメータで、'Template Actor ID'を選択します。
- * 新しく作成された全てのアクターがベースとする'デフォルト'のアクターです。
- *
- * 次に、カスタムアクターの識別方法を表す'Start ID'を設定します。
- * ゲームが開始すると、これらのIDを変更する方法はないため、
- * プロジェクトにさらにアクターを追加する予定がある場合、
- * Start IDを選択する時、それを考慮に入れる必要があります。
- * 私は安全を考慮し1000のような大きな値をお勧めします。
- *
- * -- アクターの作成 --
- *
- * 新しいアクターを作成するには、スクリプトを使用します
- *
- *   var newActor = InstanceManager.addActor()
- *
- * テンプレートアクターをベースとして使用して、新しいアクターを作成します。
- * 基本的には、新しいIDが付いたコピーです。
- *
- * 別のテンプレートをベースにしたい場合、次のスクリプトで指定できます。
- *
- *   var newActor = InstanceManager.addActor( 2 )
- *
- * テンプレートとしてアクター2を使用します。
- * アクター作成プロセスは、単にアクターを作成するだけです。
- * パーティに自動的に追加されるわけではありません。
- * したくないかもしれないからです。
- *
- * パーティーに追加したい場合
- *
- *   var newActor = InstanceManager.addActor()
- *   $gameParty.addActor(newActor.id)
- *
- * -- アクターの削除 --
- *
- * このプラグインは、他のオブジェクトによって使用されている可能性があるため、
- * アクターの削除をサポートしていません。
- * アクターを使用しない方が安全です。
- *
- * @param Template Actor ID
- * @text テンプレートアクターID
- * @desc 新しく作成される全アクターのベースとなるデフォルトのアクター
- * @default 1
- *
- * @param Start ID
- * @text 開始ID
- * @desc 新しいアクターに割り当てるID。ゲームの開始後は変更できないため、大きな値をお勧めします。
  * @default 1000
  */
 
